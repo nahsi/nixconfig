ed75da6d6748a9336c7b4aacca9728a1d19979b1
Anatolios Laskaris <github_me@nahsi.dev>
2024-11-18 00:16:47 +0200

    chore: gtk theme

--- programs/gtk.nix
+++ programs/gtk.nix
@@ -8,6 +8,14 @@
 
   gtk = {
     enable = true;
+    catppuccin = {
+      enable = true;
+      flavor = "mocha";
+      size = "compact";
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
