1b0bd6b321c519266b62c1bd0c7906653257e3b2
nahsi <nahsi@nahsi.dev>
2024-11-22 23:58:31 +0200

    chore: Fix kitty settings

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -1,1 +1,4 @@
-{ programs.qutebrowser.settings.zoom.default = "125%"; }
+{
+  programs.qutebrowser.settings.zoom.default = "125%";
+  programs.kitty.settings.font_size = 14;
+}
--- programs/kitty.nix
+++ programs/kitty.nix
@@ -2,7 +2,6 @@
   programs.kitty = {
     enable = true;
     settings = {
-      font_size = 14;
       font_family = "Fira Code";
       font_features = "Fira Code Regular +ss01 +ss02 +ss03 +ss04 +ss05 +ss07 +ss08 +zero +onum";
     };
