459f20c8455e49e16514416b88f1acb2e3252239
Anatolios Laskaris <github_me@nahsi.dev>
2026-01-22 20:26:26 +0200

    chore: Setup rctrl as compose key

--- home-manager/programs/hyprland/hyprland.nix
+++ home-manager/programs/hyprland/hyprland.nix
@@ -118,7 +118,7 @@
       #############
       input = {
         kb_layout = "us,ru";
-        kb_options = "grp:alt_shift_toggle";
+        kb_options = "grp:alt_shift_toggle,compose:rctrl";
         follow_mouse = 1;
         sensitivity = 0;
         touchpad = {
