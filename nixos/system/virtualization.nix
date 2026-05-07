288528231b0d10097dbe0fdcd1070615a41c614a
Anatolios Laskaris <github_me@nahsi.dev>
2026-05-07 22:01:46 +0300

    chore: Install quickemu

--- nixos/system/virtualization.nix
+++ nixos/system/virtualization.nix
@@ -1,9 +1,15 @@
+{ pkgs, ... }:
 {
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = [ "nahsi" ];
   virtualisation = {
     docker.enable = true;
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu.swtpm.enable = true;
+    };
     spiceUSBRedirection.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [ quickemu ];
 }
