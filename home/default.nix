73e09d1929d45cae1b440ac8dcbd9c768d76ba41
Anatolios Laskaris <github_me@nahsi.dev>
2026-06-02 18:37:01 +0300

    fix: Fix flacky asian fonts

--- home/default.nix
+++ home/default.nix
@@ -121,6 +121,7 @@ in
       # fonts
       noto-fonts
       noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       fira-code
       nerd-fonts.fantasque-sans-mono
       nerd-fonts.fira-code
@@ -139,9 +140,16 @@ in
       monospace = [
         "Fira Code"
         "FiraCode Nerd Font"
+        "Noto Sans Mono CJK JP"
       ];
-      sansSerif = [ "Fira Sans" ];
-      serif = [ "Roboto Slab" ];
+      sansSerif = [
+        "Fira Sans"
+        "Noto Sans CJK JP"
+      ];
+      serif = [
+        "Roboto Slab"
+        "Noto Serif CJK JP"
+      ];
       emoji = [ "Noto Color Emoji" ];
     };
   };
